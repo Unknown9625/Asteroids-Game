07d897cbdd551b5cdb64428aaa58481b9e7dff7a
Unknown9625 <ccmiller96@protonmail.com>
2025-03-12 18:51:58 -0400

    Added a white colored player spaceship rendering with a triangle shape

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import pygame
+import pygame # type: ignore
 
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    CLOCK = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -16,8 +18,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill(BLACK)
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = CLOCK.tick(60)/1000
 
 if __name__ == "__main__":
     main()
+
